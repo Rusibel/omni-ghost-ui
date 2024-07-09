8706c3bff81c464e6625bc7a4a23ce8c82ca9306
Polina Avdeeva <polinaavdeeva55@gmail.com>
2024-07-10 02:55:46 +0300

    feat: добавлены попапы смены скорости, статуса и батареи

--- src/pages/OmniGhostCard/OmniGhostCard.tsx
+++ src/pages/OmniGhostCard/OmniGhostCard.tsx
@@ -1,28 +1,149 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState, useCallback } from 'react';
 import styles from './styles.module.scss';
+import { useNavigate } from 'react-router-dom';
 import { ReactComponent as BackLogo } from 'src/assets/icons/back.svg';
 import { ReactComponent as DeleteLogo } from 'src/assets/icons/delete-button.svg';
 import { ReactComponent as BatteryLogo } from 'src/assets/icons/Battery.svg';
 import { ReactComponent as LockLogo } from 'src/assets/icons/lock.svg';
 import { ReactComponent as WheelLogo } from 'src/assets/icons/wheel.svg';
 import { ReactComponent as PointerLogo } from 'src/assets/icons/card-return-button.svg';
-import { MainLayout, Button } from 'src/base/components';
+import { MainLayout, Button, Popup, Input } from 'src/base/components';
 
 export const OmniGhostCard = (): ReactElement => {
+  const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
+  const [isInfoPopupOpen, setIsInfoPopupOpen] = useState<boolean>(false);
+  const [popupContent, setPopupContent] = useState<{
+    isOpen: boolean;
+    title: string;
+    inputPlaceholder: string;
+  }>({ isOpen: false, title: '', inputPlaceholder: '' });
+
+  const navigation = useNavigate();
+
+  const openInputPopup = useCallback(
+    (title: string, inputPlaceholder: string) => {
+      setPopupContent({ isOpen: true, title, inputPlaceholder });
+    },
+    [],
+  );
+
+  const closeInputPopup = useCallback(() => {
+    setPopupContent({ isOpen: false, title: '', inputPlaceholder: '' });
+  }, []);
+
+  const handleSpeedIconClick = useCallback(() => {
+    openInputPopup('Скорость движения', 'Введите скорость');
+  }, [openInputPopup]);
+
+  const handleBatteryIconClick = useCallback(() => {
+    openInputPopup('Уровень заряда батареи', 'Введите уровень заряда');
+  }, [openInputPopup]);
+
+  const handleStatusIconClick = useCallback(() => {
+    openInputPopup('Статус устройства', 'Введите новый статус');
+  }, [openInputPopup]);
+
+  const handleOpenPopup = useCallback((): void => {
+    setIsPopupOpen(true);
+  }, []);
+
+  const handleClosePopup = useCallback((): void => {
+    setIsPopupOpen(false);
+  }, []);
+
+  const handleOpenInfoPopup = useCallback((): void => {
+    setIsInfoPopupOpen(true);
+  }, []);
+
+  const handleCloseInfoPopup = useCallback((): void => {
+    setIsInfoPopupOpen(false);
+  }, []);
+
+  const handleSubmit = useCallback((): void => {
+    console.log('Submit');
+    handleClosePopup();
+  }, [handleClosePopup]);
+
+  const handleCancel = useCallback((): void => {
+    console.log('Отмена');
+    handleClosePopup();
+  }, [handleClosePopup]);
+
   return (
     <MainLayout
       topTitle="OmniGhost 55478"
       leftTopIcon={{
         svg: <BackLogo />,
-        clicked: () => console.log('Welcome'),
+        clicked: () => navigation('/main'),
       }}
       rightTopIcon={{
         svg: <DeleteLogo />,
-        clicked: () => console.log('Welcome'),
+        clicked: () => handleOpenPopup(),
       }}
       withBottomNavigation={false}
     >
       <section className={styles.card__layout}>
+        <Popup
+          isOpen={isPopupOpen}
+          handleClosePopup={handleClosePopup}
+          handleClickSubmit={handleSubmit}
+          handleCancelButtonClick={handleCancel}
+          headerTitle="Удалить OmniGhost 55478"
+          submitButtonLabel="Удалить"
+          cancelButtonLabel="Отмена"
+        >
+          <div className={styles.card__popup_text}>
+            Вы точно хотите удалить
+            <br /> OmniGhost 55478?
+          </div>
+        </Popup>
+        <Popup
+          isOpen={isInfoPopupOpen}
+          handleClosePopup={handleCloseInfoPopup}
+          handleClickSubmit={handleCloseInfoPopup}
+          handleCancelButtonClick={handleCloseInfoPopup}
+          headerTitle="OmniGhost 55478"
+          submitButtonLabel="Скопировать в буфер обмена"
+          cancelButtonLabel="Закрыть"
+        >
+          <div className={styles.card__popup_text}>
+            <ul className={styles.card__info}>
+              <li className={styles.card__info_item}>
+                Координаты:{' '}
+                <span className={styles.card__detail}>
+                  55.761811, 37.610116
+                </span>
+              </li>
+              <li className={styles.card__info_item}>
+                Статус: <span className={styles.card__detail}>Доступен</span>
+              </li>
+              <li className={styles.card__info_item}>
+                Скорость: <span className={styles.card__detail}>15 км/ч</span>
+              </li>
+              <li className={styles.card__info_item}>
+                Уровень заряда: <span className={styles.card__detail}>47%</span>
+              </li>
+              <li className={styles.card__info_item}>
+                Состояние троса:{' '}
+                <span className={styles.card__detail}>Отстёгнут</span>
+              </li>
+            </ul>
+          </div>
+        </Popup>
+        <Popup
+          isOpen={popupContent.isOpen}
+          handleClosePopup={closeInputPopup}
+          handleClickSubmit={closeInputPopup}
+          headerTitle={popupContent.title}
+          submitButtonLabel="Применить"
+        >
+          <div className={styles.card__popup_text}>
+            <Input
+              label={popupContent.title}
+              placeholder={popupContent.inputPlaceholder}
+            />
+          </div>
+        </Popup>
         <div className={styles.card_page}>
           <div className={styles.card__item}>
             <div className={styles.card__item_colm}>
@@ -34,7 +155,7 @@ export const OmniGhostCard = (): ReactElement => {
               theme="secondary"
               size="xs"
             >
-              <span>Вся информация</span>
+              <span onClick={handleOpenInfoPopup}>Вся информация</span>
             </Button>
           </div>
           <div className={styles.card__item}>
@@ -42,14 +163,14 @@ export const OmniGhostCard = (): ReactElement => {
               <h2 className={styles.card__item_title}>Онлайн</h2>
               <p className={styles.card__item_subtitle}>статус устройств</p>
             </div>
-            <PointerLogo />
+            <PointerLogo onClick={handleStatusIconClick} />
           </div>
           <div className={styles.card__item}>
             <div className={styles.card__item_colm}>
               <h2 className={styles.card__item_title}>15 км/ч</h2>
               <p className={styles.card__item_subtitle}>скорость движения</p>
             </div>
-            <PointerLogo />
+            <PointerLogo onClick={handleSpeedIconClick} />
           </div>
           <div className={styles.card__item}>
             <div className={styles.card__item_colm}>
@@ -62,7 +183,7 @@ export const OmniGhostCard = (): ReactElement => {
                 уровень заряда батареи
               </p>
             </div>
-            <PointerLogo />
+            <PointerLogo onClick={handleBatteryIconClick} />
           </div>
           <div className={styles.card__item}>
             <div className={styles.card__item_colm}>
